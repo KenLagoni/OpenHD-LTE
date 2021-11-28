43959f3590e939a7749984261e68da274af587d4
Kenneth Lagoni Olsen <kenneth@lagoni.org>
2021-11-28 11:31:13 +0100

    Air start scripts updated to be split into main and mainpart2 for more option on firmware updated.

--- air/main.sh
+++ air/main.sh
@@ -3,118 +3,23 @@
 
 source $1/air-settings.sh
 
-
-#LOGPATH="/home/pi/OpenHD-LTE"
-#LOGFILE="test.log"
+## keep it short since this part will only update after next reboot.
 
 
 mkdir -p $MOUNTPATH
-mount /dev/mmcblk0p3 $MOUNTPATH
+chmod 777 $MOUNTPATH 
+mount -o rw,users,umask=000 /dev/mmcblk0p3 $MOUNTPATH
+#mount /dev/mmcblk0p3 $MOUNTPATH 
 
-mkdir -p /var/run/openhd
-mkfifo /var/run/openhd/videofifo
-
-#mkdir -p $LOGPATH
-
-
-LOGNUMBER=0
-if [ -f $MOUNTPATH/lastlog.txt ]
+echo -n "Checking if firmware is located on SD card..."
+if [ -f $MOUNTPATH/$FIRMWARE ] ## Check for firmware on SD, this must be used if there.
 then
-	LOGNUMBER=`cat $MOUNTPATH/lastlog.txt`
-else
-	echo "0" > $MOUNTPATH/lastlog.txt
+    echo "Yes!"
+	mv $MOUNTPATH/$FIRMWARE $INSTALLPATH/$FIRMWARE
+    chown $USER:$USER $INSTALLPATH/$FIRMWARE
+    tar -xzvf $INSTALLPATH/$FIRMWARE -C 
+    chown $USER:$USER $INSTALLPATH -R 
 fi
 
-let LOGNUMBER++
-echo "$LOGNUMBER" > $MOUNTPATH/lastlog.txt
-FOLDER=$(printf "%06d" $LOGNUMBER)
-
-DATAFOLDER=$MOUNTPATH/$FOLDER
-mkdir $DATAFOLDER
-
-LOGFOLDER=$DATAFOLDER/log
-LOG=$LOGFOLDER/$LOGFILE
-mkdir $LOGFOLDER
-
-
-
-
-#gpio mode 25 in
-# Set the variable through command substitution
-#mode=$(gpio read 25)
-
-
-#if [ $mode -gt 0 ]
-#then
-	# Test-Bech!
-#	echo "$(date)   -   PowerUp in test bech"
-	
-#	start_camera
-	
-#else
-	# Drone!
-		#Rotate log
-
-
-    # Drone!
-   # echo "$(date)   -   Power Up as drone" >> $LOG
-#	echo "$(date)   -   Starting OpenHD-LTE $VERSION" >> $LOG
-#	echo "$(date)   -   Setting up LTE modem:" >> $LOG
-	
-	echo "Setting up LTE modem..." | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
-	ip link set wwan0 down 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
-	echo 'Y' | sudo tee /sys/class/net/wwan0/qmi/raw_ip 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
-	sudo ip link set wwan0 up 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
-	sudo qmicli -d /dev/cdc-wdm0 --wda-get-data-format 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
-	sudo qmicli -p -d /dev/cdc-wdm0 --device-open-net='net-raw-ip|net-no-qos-header' --wds-start-network="apn='$APN',ip-type=4" --client-no-release-cid 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
-	sudo udhcpc -q -f -i wwan0 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG	
-#	$AIRSTARTSCRIPT/4gConnect.sh $AIRSTARTSCRIPT 2>&1 >> $LOG 
-	echo "LTE modem setup complete." | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
-	# echo "$(date)   -   LTE modem setup complete" >> $LOG
-
-	sleep 2
-	
-	echo "Starting OpenHD-LTE Air side version $VERSION" | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
-	echo "Updating Firmware...." | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
-	$AIRSTARTSCRIPT/firmwareUpdate.sh $AIRSTARTSCRIPT 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
-
-	sleep 1
-
-	## Test Camera:
-	echo "Testing camera:" | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
-	raspistill -o test.jpg 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
-
-	#$AIRSTARTSCRIPT/startCamera.sh $AIRSTARTSCRIPT $LOG &
-	#sleep 1
-	$AIRSTARTSCRIPT/startTXraw.sh $AIRSTARTSCRIPT $LOG $DATAFOLDER &
-	
-	cp /var/log/messages $LOGFOLDER/messages
-	dd if=/dev/null of=/var/log/messages
-	tail -f -n 0 /var/log/messages >> $LOGFOLDER/messages &
-
-	cp /var/log/syslog $LOGFOLDER/syslog
-	dd if=/dev/null of=/var/log/syslog
-	tail -f -n 0 /var/log/syslog >> $LOGFOLDER/syslog &
-
-	cp /var/log/daemon.log $LOGFOLDER/daemon.log
-	dd if=/dev/null of=/var/log/daemon.log
-    tail -f -n 0 /var/log/daemon.log >> $LOGFOLDER/daemon.log &
-
-	cp /var/log/kern.log $LOGFOLDER/kern.log
-	dd if=/dev/null of=/var/log/kern.log
-    tail -f -n 0 /var/log/kern.log >> $LOGFOLDER/kern.log &
-
-    cp /var/log/auth.log $LOGFOLDER/auth.log
-    dd if=/dev/null of=/var/log/auth.log
-    tail -f -n 0 /var/log/auth.log >> $LOGFOLDER/auth.log &
-
-    cp /var/log/user.log $LOGFOLDER/user.log
-    dd if=/dev/null of=/var/log/user.log
-    tail -f -n 0 /var/log/user.log >> $LOGFOLDER/user.log &
-
-    cp /var/log/debug $LOGFOLDER/debug
-    dd if=/dev/null of=/var/log/debug
-    tail -f -n 0 /var/log/debug >> $LOGFOLDER/debug &
-
-	sync
-#fi
+echo "No"
+$AIRSTARTSCRIPT/mainPart2.sh $AIRSTARTSCRIPT
--- air/startTXraw.sh
+++ air/startTXraw.sh
@@ -5,11 +5,16 @@ source $1/air-settings.sh
 LOG=$2
 VIDEOSAVEPATH=$3
 
+## Test Camera:
+echo "Testing camera:" | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
+raspistill -o test.jpg 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
+
+
 for (( ; ; ))
 do
-	echo "Starting TX_RAW to IP=$IP VIDEO_PORT=$VIDEOPORT MAVLINK_PORT=$MAVLINKPORT TELEMETRY_PORT=$TELEMETRIPORT with max recording size of "$FILEMAX"MB." | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
-	raspivid -w $WIDTH -h $HEIGTH -fps $FPS -b $BITRATE  -t 0 -o - | $AIRSTARTSCRIPT/tx_raw -i $GROUND_IP -v $VIDEOPORT -s /dev/serial0 -p $MAVLINKPORT -t $TELEMETRIPORT -o $VIDEOSAVEPATH/record -z $FILEMAX 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG 
-	#cat /var/run/openhd/videofifo | $AIRSTARTSCRIPT/tx_raw -i $GROUND_IP -v $VIDEOPORT -s /dev/serial0 -p $MAVLINKPORT -t $TELEMETRIPORT -o $VIDEOSAVEPATH/record -z $FILEMAX 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG 
+	echo "Starting TX_RAW to IP=$IP VIDEO_PORT=$VIDEOPORT MAVLINK_PORT=$MAVLINKPORT TELEMETRY_PORT=$TELEMETRIPORT" | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
+	#raspivid -w $WIDTH -h $HEIGTH -fps $FPS -b $BITRATE  -t 0 -o - | $AIRSTARTSCRIPT/tx_raw -i $GROUND_IP -v $VIDEOPORT -s /dev/serial0 -p $MAVLINKPORT -t $TELEMETRIPORT -o $VIDEOSAVEPATH 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG 
+	cat /var/run/openhd/videofifo | $AIRSTARTSCRIPT/tx_raw -i $GROUND_IP -v $VIDEOPORT -s /dev/serial0 -p $MAVLINKPORT -t $TELEMETRIPORT -o $VIDEOSAVEPATH 2>&1 | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG 
 	echo "TX_RAW - Crashed! - Restarting in 10 seconds..." | ts '[%Y-%m-%d %H:%M:%S]' >> $LOG
     sleep 10
 done
